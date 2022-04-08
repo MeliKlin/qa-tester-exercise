4491ad0e10679861913da8aab63a02fe09f09db1
Klinton Sae Hyung Lee <klinton.lee@mercadolivre.com>
2022-04-08 16:58:18 -0300

    chore: add initial structure

---
+++ src/test/java/com/mercadolivre/qabugs/QaBugsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercadolivre.qabugs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QaBugsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
